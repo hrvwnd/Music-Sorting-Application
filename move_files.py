f9bd72d6b521e2f690342950c7047685e08eb7b4
hrvwnd <harveyawendon@gmail.com>
2019-11-27 20:51:24 +0000

    Added SQL files, edited move_files for work from home on laptop

--- move_files.py
+++ move_files.py
@@ -4,18 +4,24 @@ import shutil
 import os
 
 def move_files():
-    #path1 = "C:\\Users\\harve\\Documents\\NEW_qa_coding\\project_music_sort\\music\\liquid.mp3"
-    path1 = "C:\\Users\\Admin\\Desktop\\try_to_use_git\\harvey._assessment\\music\\liquid.mp3"
-    path2 = "C:\\Users\\Admin\\Desktop\\try_to_use_git\\harvey._assessment\\music\\liquid\\liquid.mp3"
-    #path2 = 'C:\\Users\\harve\\Documents\\NEW_qa_coding\\project_music_sort\\music\\liquid\\liquid.mp3'
+    path1 = "C:\\Users\\harve\\Documents\\qacoding\\harvey._assessment\\music\\liquid.mp3"
+    #path1 = "C:\\Users\\Admin\\Desktop\\try_to_use_git\\harvey._assessment\\music\\liquid.mp3"
+    #path2 = "C:\\Users\\Admin\\Desktop\\try_to_use_git\\harvey._assessment\\music\\liquid\\liquid.mp3"
+    path2 = "C:\\Users\\harve\\Documents\\qacoding\\harvey._assessment\\music\\liquid\\liquid.mp3"
     #src = "/C:/Users/harve/Documents/NEW_qa_coding/project_music_sort-/music/liquid.mp3"
     #dst = "/C:/Users/harve/Documents/NEW_qa_coding/project_music_sort-/music/liquid/liquid.mp3"
     #path1 = "\\music\\liquid.mp3"
     #path2 = "\\music\\liquid\\liquid.mp3"
     #path1 = "C:\\Users\\Admin\\Desktop\\try_to_use git\\project_music_sort\\music\\liquid.mp3"
     #path2 = "C:\\Users\\Admin\\Desktop\\try_to_use git\\project_music_sort\\music\\liquid"
-    os.rename(path1, path2)
-    #shutil.move(path1,path2)
+    try:
+        os.rename(path1, path2)
+    except FileNotFoundError:
+        print ("Error: Check file directories are correct: \n", path1, "\n", path2)
+    try:
+        shutil.move(path1,path2)
+    except FileNotFoundError:
+        print ("Error: Check file directories are correct: \n", path1, "\n", path2)
 move_files()
 
 
