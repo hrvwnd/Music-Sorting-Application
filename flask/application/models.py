d2f0a6d7531abc15e1f03c935620441a237a4cde
hrvwnd <harveyawendon@gmail.com>
2019-12-03 17:58:56 +0000

    added flask files with start models.py tables

---
+++ flask/application/models.py
@@ -0,0 +1,74 @@
+from application import db,login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+class Users(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable = False)
+    email = db.Column(db.String(150),nullable=False,unique=True)
+    password = db.Column(db.String(200),nullable=False)
+    posts = db.relationship('Posts', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return ''.join([
+            'User ID: ',str(self.id), '\r\n',
+            'Email: ', self.email,
+            'Name', self.first_name,' ', self.last_name
+            ])
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    content = db.Column(db.String(100), nullable=False, unique=True)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    def __repr__(self):
+        return ''.join([
+            'User_ID: ', self.first_name, ' ', self.last_name, '\r\n',
+            'Title: ', self.title, '\r\n', self.content
+        ])
+
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+
+
+#project tables start 
+    
+class Artists(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, unique = True)
+    name = db.Column(db.String(100), nullable=False, unique= True)
+    default_genre = db.Column(db.String(100),nullable = False, unique= True)
+    def __repr__(self):
+        return ''.join([
+            'Artist_ID', self.name,'\r\n',
+            'Default_Genre: ', self.default_genre, '\r\n'
+        ])
+
+class Genres(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, unique = True)
+    name = db.Column(db.String(100), nullable = False, unique = True)
+    folder_path = db.Column(db.String(100), nullable = False, unique = True)
+    def __repr__   (self):
+        return ''.join([
+            'Genre_ID', self.name,'\r\n',
+            'Folder_path: ', self.folder_path, '\r\n'
+        ])
+
+class Tracks(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique = True)
+    title = db.Column(db.String(100), nullable = False)
+    filename = db.Column(db.String(100), nullable = False, unique = True)
+    album = db.Column(db.String(100), nullable = False)
+    artist_id = db.column(db.Integer,db.ForeignKey('artists.id)'))
+    genre_id = b.column(db.Integer,db.ForeignKey('genres.id'))
+    def __repr__(self):
+        return ''.join([
+            'Tracks_ID', self.title, self.album, '\r\n'
+            'Filename', self.filename, '\r\n'
+        ])
+
