9657d5e07aa4cf2b534beda395b4f917c982d291
hrvwnd <harveyawendon@gmail.com>
2019-12-04 16:20:41 +0000

    Update models.py

--- flask/application/models.py
+++ flask/application/models.py
@@ -38,37 +38,24 @@ def load_user(id):
 
 
 #project tables start 
-    
+"""
 class Artists(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, unique = True)
     name = db.Column(db.String(100), nullable=False, unique= True)
     default_genre = db.Column(db.String(100),nullable = False, unique= True)
-    def __repr__(self):
-        return ''.join([
-            'Artist_ID', self.name,'\r\n',
-            'Default_Genre: ', self.default_genre, '\r\n'
-        ])
 
 class Genres(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, unique = True)
     name = db.Column(db.String(100), nullable = False, unique = True)
     folder_path = db.Column(db.String(100), nullable = False, unique = True)
-    def __repr__   (self):
-        return ''.join([
-            'Genre_ID', self.name,'\r\n',
-            'Folder_path: ', self.folder_path, '\r\n'
-        ])
 
 class Tracks(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique = True)
     title = db.Column(db.String(100), nullable = False)
     filename = db.Column(db.String(100), nullable = False, unique = True)
     album = db.Column(db.String(100), nullable = False)
-    artist_id = db.column(db.Integer,db.ForeignKey('artists.id)'))
-    genre_id = b.column(db.Integer,db.ForeignKey('genres.id'))
-    def __repr__(self):
-        return ''.join([
-            'Tracks_ID', self.title, self.album, '\r\n'
-            'Filename', self.filename, '\r\n'
-        ])
+    artist_id = db.Column(db.Integer,db.ForeignKey('artists.id)'))
+    genre_id = db.Column(db.Integer,db.ForeignKey('genre.id'))
+
+"""
 
