098cbb820cb86773ae8be444b55dbe7ff2dfa70b
hrvwnd <harveyawendon@gmail.com>
2019-12-05 12:47:04 +0000

    testing eyed3

--- py/test_eyed3.py
+++ py/test_eyed3.py
@@ -1,6 +1,22 @@
 import sys
 import eyed3
 
-file = eyed3.load("test.mp3")
-print (file.tag.artist)
-print (file.tag.title)
+try:
+    file = eyed3.load("/home/harveyawendon/harvey/music/test.mp3")
+    artist = file.tag.artist
+    title = file.tag.title
+except AttributeError:
+    print ("file does not have id3 tags to read")
+else:
+    print (file.tag.artist)
+    print (file.tag.title)
+    
+try:    
+    file = eyed3.load("/home/harveyawendon/harvey/music/hiphop.mp3")
+    artist = file.tag.artist
+    title = file.tag.title
+except AttributeError:
+    print ("file does not have id3 tags")
+else:
+    print (file.tag.artist)
+    print (file.tag.title)
