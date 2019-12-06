272d34c96f87f2bb7b318a14b5633a3217b06c29
hrvwnd <harveyawendon@gmail.com>
2019-12-06 15:44:29 +0000

    Added eyed3 splitter to remove annoying text that prevented post route working

--- application/routes.py
+++ application/routes.py
@@ -119,6 +119,7 @@ def sort():
         for mp3 in mp3s:
             track_id3_tags = mp3_id3_read(directory,mp3) # returns id3 tags of track
             id3_artist = track_id3_tags[2] #selects artist (title, album, artist, genre)
+            ed3_artist = id3_artist.lower
             artist_in_db = bool(Artists.query.filter_by(name=id3_artist).first())
             print (id3_artist)
             print (artist_in_db)
@@ -134,6 +135,7 @@ def sort():
                 elif artist_has_no_default_genre == False:
                     artist_dbrecord = Artists.query.filter_by(name=id3_artist)
                     print (artist_dbrecord)
+
                     #--------------------CHANGEME---------------------------------
                     #existing_folder = folder_identify_lx(directory,CHANGEME)
                     #if existing_folder:
