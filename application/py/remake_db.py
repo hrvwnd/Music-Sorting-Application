303fff0d1e817809aaf4f54583e2493c69117817
hrvwnd <harveyawendon@gmail.com>
2019-12-08 20:05:02 +0000

    fixing talbes to have the flask app file directory

--- application/py/remake_db.py
+++ application/py/remake_db.py
@@ -1,28 +1,28 @@
-    def remake_db(self):
-        #creates and drops database
-        # Will be called for every test 
+def remake_db(self):
+    #creates and drops database
+    # Will be called for every test 
 
-        db.session.commit()
-        db.drop_all()
-        db.create_all()
+    db.session.commit()
+    db.drop_all()
+    db.create_all()
 
-        #creates test artists
-        artist1 = Artists(name = "keeno", default_genre = "liquid")
-        artist2 = Artists(name = "dilated peoples", default_genre = "hiphop")
+    #creates test artists
+    artist1 = Artists(name = "keeno", default_genre = "liquid")
+    artist2 = Artists(name = "dilated peoples", default_genre = "hiphop")
 
-        
-        genre1 = Genres(name="liquid", folder_path = "/opt/flask-app/music/liquid")
-        genre2 = Genres(name = "hiphop", folder_path = "/opt/flask-app/music/hiphop")
-        
-        track1 = Tracks(title = "You Can't Hide, You Can't Run (prod. by Evidence)", filename = "test.mp3", album = "20/20", artist_id = "2",genre_id = "2")
-        track2 = Tracks(title = "Guesswork", filename = "test2.mp3", album = "All The Shimmering Things", artist_id = "1", genre_id = "1")
-        #saves users to database
+    
+    genre1 = Genres(name="liquid", folder_path = "/opt/flask-app/music/liquid")
+    genre2 = Genres(name = "hiphop", folder_path = "/opt/flask-app/music/hiphop")
+    
+    track1 = Tracks(title = "You Can't Hide, You Can't Run (prod. by Evidence)", filename = "test.mp3", album = "20/20", artist_id = "2",genre_id = "2")
+    track2 = Tracks(title = "Guesswork", filename = "test2.mp3", album = "All The Shimmering Things", artist_id = "1", genre_id = "1")
+    #saves users to database
 
-        db.session.add(artist1)
-        db.session.add(artist2)
-        db.session.add(genre1)
-        db.session.add(genre2)
-        db.session.add(track1)
-        db.session.add(track2)
+    db.session.add(artist1)
+    db.session.add(artist2)
+    db.session.add(genre1)
+    db.session.add(genre2)
+    db.session.add(track1)
+    db.session.add(track2)
 
-        db.session.commit()
+    db.session.commit()
