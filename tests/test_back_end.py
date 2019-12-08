d562a6f6af8ff82042fce341e5f8548fec5ddcfb
hrvwnd <harveyawendon@gmail.com>
2019-12-08 17:44:39 +0000

    adding create tables for testing

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -7,11 +7,12 @@ from application.models import Tracks, Artists, Genres
 
 class UnitBase(TestCase):
 
-     def create_app(self):
+    def create_app(self):
         #pass in test configurations
         config_name = "testing"
         app.config.update(
-                SQLALCHEMY_DATABASE_URI='mysql+pymysql://'+str(getenv('MYSQL_USER'))+':'+str(getenv('MYSQL_PASSWORD'))+'@'+str(getenv('MYSQL_HOST'))+'/'+str(getenv('MYSQL_DB_TEST'))
+                SQLALCHEMY_DATABASE_URI='mysql+pymysql://'+str(getenv('MYSQL_USER'))+':' \
+                    +str(getenv('MYSQL_PASSWORD'))+'@'+str(getenv('MYSQL_HOST'))+'/'+str(getenv('MYSQL_DB_TEST')))
         return app
 
     def create_app(self):
