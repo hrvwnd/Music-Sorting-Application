b3f23ebb3dd0eb318059d6343def7012bbd56dfe
hrvwnd <harveyawendon@gmail.com>
2019-12-08 19:55:33 +0000

    fixing talbes to have the flask app file directory

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -4,17 +4,18 @@ from flask_testing import TestCase
 from os import getenv
 from application import app, db
 from application.models import Tracks, Artists, Genres
-
-class UnitBase(TestCase):
 """
     def create_app(self):
         #pass in test configurations
         config_name = "testing"
         app.config.update(
-                SQLALCHEMY_DATABASE_URI='mysql+pymysql://'+str(getenv('MYSQL_USER'))+':' \
-                    +str(getenv('MYSQL_PASSWORD'))+'@'+str(getenv('MYSQL_HOST'))+'/'+str(getenv('MYSQL_DB_TEST')))
-        return app
-"""
+            SQLALCHEMY_DATABASE_URI='mysql+pymysql://'+str(getenv('MYSQL_USER'))+':' \
+                +str(getenv('MYSQL_PASSWORD'))+'@'+str(getenv('MYSQL_HOST'))+'/'+str(getenv('MYSQL_DB_TEST')))
+        return app """
+        
+class UnitBase(TestCase):
+
+
     def setup(self):
         #creates and drops database
         # Will be called for every test 
