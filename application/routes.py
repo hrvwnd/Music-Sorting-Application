a3eafc5d2c4da92d7c3289a629ef21d80631aee7
hrvwnd <harveyawendon@gmail.com>
2019-12-09 07:23:01 +0000

    fixing a few validation fields to better fit their role

--- application/routes.py
+++ application/routes.py
@@ -14,7 +14,7 @@ from application.py.file_interactions import create_single_folder, identify_mp3_
 @app.route('/sort', methods = ['GET','POST'])
 def sort():
     #directory = "/home/harveyawendon/harvey/music" #testing store (only works on hosted vm)
-    directory = "/opt/flask-app/music"
+    directory = "/opt/flask-app/music/"
     mp3s = identify_mp3_lx(directory)
     form = SortForm()
     print (str(mp3s))
