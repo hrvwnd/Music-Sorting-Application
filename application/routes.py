695bcb9b8d1e1d7bc83c3361be8e01c95a0f3ed3
hrvwnd <harveyawendon@gmail.com>
2019-12-09 12:17:10 +0000

    fixing bug where folders aren't created inside linux

--- application/routes.py
+++ application/routes.py
@@ -91,7 +91,7 @@ def delete():
     form = DeleteASong()
     if form.validate_on_submit():
         Tracks.query.filter_by(title = form.song_title.data).delete()
-        flash (str(form.song_title.data) + "Song Deleted")
+        #flash (str(form.song_title.data) + "Song Deleted")
         return redirect(url_for('delete'))
     return render_template("delete.html",title="Delete a song", form=form)
 
